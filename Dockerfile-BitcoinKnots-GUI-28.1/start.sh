a51306e533190086fac9e3bd4c5b8e0f985d3ba9
Serge <sv1slim@gmail.com>
2025-07-17 13:22:35 -0400

    Update start.sh
    changing main location of the bitcoin to make sure getblockchaininfo commands are working

--- Dockerfile-BitcoinKnots-GUI-28.1/start.sh
+++ Dockerfile-BitcoinKnots-GUI-28.1/start.sh
@@ -6,7 +6,7 @@ rm -f /tmp/.X0-lock             # ensure clean start
 Xvfb :0 -screen 0 1280x800x16 & # virtual display
 fluxbox &                       # tiny window manager
 
-bitcoin-qt -datadir=/bitcoin -conf=/bitcoin/bitcoin.conf &  # GUI
+bitcoin-qt -datadir=/root/.bitcoin -conf=/root/.bitcoin/bitcoin.conf &  # GUI
 
 x11vnc  -display :0 -forever -nopw -shared -rfbport 5900 &  # raw VNC
 /opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &  # browser VNC
